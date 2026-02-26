e0528c90ea1764e88536167425f294ee84082122
FxDamian <yofelix473@gmail.com>
2026-02-26 17:59:03 -0500

    add 1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -25,26 +25,28 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-    name: 'Fundamentals of React',
-    exercises:10
-    },
-    {
-    name: 'Using props to pass data',
-    exercises:7
-    },
-    {
-    name: 'State of a component',
-    exercises:14
-    }
-  ]
+  const course ={
+    name: 'Half Stack application development',
+    parts:[
+      {
+      name: 'Fundamentals of React',
+      exercises:10
+      },
+      {
+      name: 'Using props to pass data',
+      exercises:7
+      },
+      {
+      name: 'State of a component',
+      exercises:14
+      }
+    ]
+  }
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={[] =parts.map(part => part.name)} exercises={[] = parts.map(part => part.exercises)}/>
-      <Total exercises = {[] = parts.map(part => part.exercises)}/>
+      <Header course={course.name}/>
+      <Content parts={[] =course.parts.map(part => part.name)} exercises={[] = course.parts.map(part => part.exercises)}/>
+      <Total exercises = {[] = course.parts.map(part => part.exercises)}/>
     </div>
   )
 }

1cc69f1b1dbc9870ccbc1c86a214fde14fcccccf
FxDamian <yofelix473@gmail.com>
2026-02-27 15:55:12 -0500

    add 1.11

--- part1/d_A_more_complex_state__debugging_React_apps/src/App.jsx
+++ part1/d_A_more_complex_state__debugging_React_apps/src/App.jsx
@@ -5,22 +5,23 @@ const Button = ({onClick,text}) =>{
 }
 const StatisticsLine = ({text,value}) =>{
   return(
-    <>
-    <p>{text} {value}</p>
-    </>
+    <tr>
+      <td>{text}</td> 
+      <td>{value}</td>
+    </tr>
   )
 }
 const Statistics = ({good,neutral,bad,all,average,positive}) =>{
   if(all>0){
     return(
-      <>
+      <table>
         <StatisticsLine text={'good'} value = {good} />
         <StatisticsLine text={'neutral'} value = {neutral} />
         <StatisticsLine text={'bad'} value = {bad} />
         <StatisticsLine text={'all'} value = {all} />
         <StatisticsLine text={'average'} value = {average} />
         <StatisticsLine text={'positive %'} value ={positive}/>
-      </>
+      </table>
     )
   }else{
     return <h2>No feedback given</h2>
